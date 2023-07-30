333d936b25cbd8355bd3c606e4c27f4ca7b91718
Cameron Henshaw <cameronhenshaw@Camerons-MacBook-Pro.local>
2023-07-30 10:53:06 -0400

    adding styling and breaking up components

---
+++ src/components/Info/Info.jsx
@@ -0,0 +1,10 @@
+function Info() {
+  return (
+    <div>
+      <h2> Information about Legislation in 'state'</h2>
+      <p> This is where a bunch of nice info will show up.</p>
+    </div>
+  );
+}
+
+export default Info;
